34b57dc7e6b3f21ddebfe2e694d287b048b12411
v1centebarros <vmabarros@ua.pt>
2024-08-07 11:11:12 +0100

    Add the codebase for the autentication working in the backend

---
+++ backend/app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.dependencies.database import Base, engine
+from app.routes import auth
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+app.include_router(auth.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
