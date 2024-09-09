797598074578632182e2bbbecbbb19e7233a53ea
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2024-09-09 15:57:46 +0100

    chore: Update colors in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      "blue-primary": "#272654",
+      "brown-primary": "#8B4615",
+      "gray-primary": "#5F5F5F",
+      "gray-secondary": "#5F5F5F99",
+      "black-primary":"#292929",
+      "white":"#FFFFFF",
+    },
+    extend: {
+
+    },
   },
   plugins: [
     require('daisyui')
